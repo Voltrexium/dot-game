12f8c03ec9fd30dea2275d825d3807a8019e74a9
EnyCo <enay@netventix.com>
2026-06-29 21:20:04 -0500

    reset key syste,

--- config.example.js
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = "https://your-project.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_your_key_here";
---
+++ config.js
@@ -0,0 +1,2 @@
+export const SUPABASE_URL = "https://vydzuwlehgcozhunljxh.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_HZZD4d__fKYYkCmcrOxsZQ_yqmLRWim";
